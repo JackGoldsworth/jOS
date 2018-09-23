0d6dadf925e73720d9bed010c97170c4bd47beb6
JackGoldsworth <jackgoldsworth@gmail.com>
2018-09-23 17:10:38 -0500

    Basic boot script

---
+++ boot/bootsect.asm
@@ -0,0 +1,18 @@
+; Basically just copy pasted code just to get a start on things
+; Need to look more into assembly to really get going.
+mov ah, 0x0e ; tty mode
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10 ; 'l' is still on al, remember?
+mov al, 'o'
+int 0x10
+
+jmp $ ; jump to current address = infinite loop
+
+; padding and magic number
+times 510 - ($-$$) db 0
+dw 0xaa55 
