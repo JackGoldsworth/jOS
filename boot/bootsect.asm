ca50d6be1b2ecaf72eb43c25c72e24d1f08eb036
JackGoldsworth <jackgoldsworth@gmail.com>
2018-10-04 14:40:47 -0500

    Messing around. I understand all of this now though!

--- boot/bootsect.asm
+++ boot/bootsect.asm
@@ -1,18 +1,13 @@
-; Basically just copy pasted code just to get a start on things
-; Need to look more into assembly to really get going.
-mov ah, 0x0e ; tty mode
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10 ; 'l' is still on al, remember?
-mov al, 'o'
+[bits 16]
+[org 0x7c00] ; Location of the boot sector
+
+mov ah, 0x0e
+mov al, [msg]
 int 0x10
 
-jmp $ ; jump to current address = infinite loop
+jmp $
 
-; padding and magic number
-times 510 - ($-$$) db 0
-dw 0xaa55 
+msg db "X", 10 ; Can only print one character
+
+times 510-($-$$) db 0 ; pads 510 bytes with 0
+dw 0xaa55 ; Adds boot number to last 2 bytes
